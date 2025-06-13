90bfcb96dae3090ffa321c977fa057fcb48ef2d0
Nicola <nsprogramwork@mail.com>
2025-06-14 01:44:09 +0200

    ancora primo commit

---
+++ src/main/java/it/app/contabilita/ContabilitaApplication.java
@@ -0,0 +1,13 @@
+package it.app.contabilita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/app/contabilita/ContabilitaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.app.contabilita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
